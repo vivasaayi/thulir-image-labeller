143374f63435c7b533e539de0ca82142deb8e04e
Rajan Panneer Selvam <vivasaayi@live.com>
2022-06-16 05:19:23 -0400

    Render Images

--- ImageLabeller/Controllers/ImageController.cs
+++ ImageLabeller/Controllers/ImageController.cs
@@ -9,11 +9,32 @@ namespace ImageLabeller.Controllers;
 public class ImageController : ControllerBase
 {
     private LabelsRepository _labelsRepository = new LabelsRepository();
+    private ImagesRepository _imagesRepository = new ImagesRepository();
     
     [HttpGet]
-    public async Task<string> Get()
+    public async Task<List<Image>> Get()
     {
-        return "new string 4";
+        return _imagesRepository.GetImageNames();
+    }
+    
+    [HttpGet("next-image-info")]
+    public async Task<Image> GetNextImage(int index)
+    {
+        var images = _imagesRepository.GetImageNames();
+
+        return images[index++];
+    }
+    
+    [HttpGet("render-image")]
+    public async Task<FileContentResult> RenderImage(int index)
+    {
+        var images = _imagesRepository.GetImageNames();
+
+        var imagedetails = images[index++];
+
+        var imageBytes = System.IO.File.ReadAllBytes(imagedetails.ImageLocation);  
+        
+        return File(imageBytes, "image/jpeg");
     }
     
     [HttpPost]
